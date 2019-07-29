9efc4711472f6fef4e2fa96c097d5544776de381
Philipp Ossler <philipp.ossler@gmail.com>
2019-07-29 07:10:06 +0200

    chore(msgpack): parse condition with FastParse

--- msgpack-el/src/main/scala/io/zeebe/msgpack/MessagePackParser.scala
+++ msgpack-el/src/main/scala/io/zeebe/msgpack/MessagePackParser.scala
@@ -2,6 +2,8 @@ package io.zeebe.msgpack
 
 import fastparse.NoWhitespace._
 import fastparse._
+import io.zeebe.msgpack.spec.{MsgPackToken, MsgPackType}
+import io.zeebe.util.buffer.BufferUtil
 
 object MessagePackParser {
 
@@ -25,10 +27,60 @@ object MessagePackParser {
     )
   }
 
-  private def name[_: P]: P[String] = P((nameStart ~ namePart.rep).!)
+  private def name[_: P]: P[String] = P((nameStart ~ namePart.rep)).!
 
-  private def nameStart[_: P]: P[String] = P(CharIn("a-zA-Z").!)
+  private def nameStart[_: P]: P[String] = P(CharIn("a-zA-Z")).!
 
-  private def namePart[_: P]: P[String] = P(CharIn("a-zA-Z0-9_").!)
+  private def namePart[_: P]: P[String] = P(CharIn("a-zA-Z0-9_")).!
+
+  private def condition[_: P] = ???
+
+  private def comparison[_: P] = P((literal | query) ~ ("==" | "!=" | "<" | "<=" | ">" | ">=").! ~ (literal | query)).map {
+    case (x: MsgPackToken, "==", y: MessagePackQuery) => ???
+  }
+
+  private def literal[_: P]: P[MsgPackToken] = P(nullLiteral | booleanLiteral | stringLiteral | numberLiteral)
+
+  private def nullLiteral[_: P]: P[MsgPackToken] = P("null").map(_ => MsgPackToken.NIL)
+
+  private def booleanLiteral[_: P]: P[MsgPackToken] = P("true" | "false").!.map { b =>
+    val token = new MsgPackToken
+    token.setType(MsgPackType.BOOLEAN)
+    token.setValue(b.toBoolean)
+    token
+  }
+
+  private def stringLiteral[_: P]: P[MsgPackToken] = P(
+    ("\"" ~ CharsWhile(_ != '\"').! ~ "\"")
+      | ("\'" ~ CharsWhile(_ != '\'').! ~ "\'")).map { s =>
+
+    val buffer = BufferUtil.wrapString(s)
+
+    val token = new MsgPackToken()
+    token.setType(MsgPackType.STRING)
+    token.setValue(buffer, 0, buffer.capacity())
+    token
+  }
+
+  private def numberLiteral[_: P]: P[MsgPackToken] = P(CharIn("+\\-").? ~ integral ~ fractional.?).!.map { n =>
+    val x = n.toDouble
+
+    val token = new MsgPackToken();
+    if (x.isWhole) {
+      token.setType(MsgPackType.INTEGER)
+      token.setValue(x.toLong)
+    } else {
+      token.setType(MsgPackType.FLOAT)
+      token.setValue(x)
+    }
+    token
+  }
+
+  private def fractional[_: P] = P("." ~ digits)
+
+  private def integral[_: P] = P("0" | CharIn("1-9") ~ digits.?)
+
+  private def digits[_: P] = P(CharsWhileIn("0-9"))
+
 
 }
---
+++ msgpack-el/src/main/scala/io/zeebe/msgpack/MessagePackResult.scala
@@ -0,0 +1,9 @@
+package io.zeebe.msgpack
+
+import org.agrona.DirectBuffer
+import org.agrona.concurrent.UnsafeBuffer
+
+class MessagePackResult(val buffer: DirectBuffer = new UnsafeBuffer(0, 0)) {
+
+
+}
